8a64af61f539754d167397295e6f702c9125f357
visstt <egorskomorohov020606@gmail.com>
2025-02-16 18:52:22 +0500

    adds

--- src/store/heroStore.js
+++ src/store/heroStore.js
@@ -27,6 +27,22 @@ const useHeroStore = create((set, get) => ({
       );
     }
   },
+  addHeroMyApi: async (formData) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:3000/hero/addPerson`,
+        {
+          formData,
+        }
+      );
+      console.log("Успешное добавление солдата:", response.data);
+    } catch (error) {
+      console.log(
+        "Ошибка добавления солдата:",
+        error.response?.data || error.message
+      );
+    }
+  },
   filters: [],
   getFilter: async () => {
     try {

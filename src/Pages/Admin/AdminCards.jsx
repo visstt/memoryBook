dd345ed075b6058199fd0dadee19c81b3e8c51fb
visstt <egorskomorohov020606@gmail.com>
2025-02-16 19:44:49 +0500

    finals

--- src/Pages/Admin/AdminCards.jsx
+++ src/Pages/Admin/AdminCards.jsx
@@ -6,6 +6,7 @@ import {
   CardMedia,
   Container,
   Typography,
+  Grid2,
 } from "@mui/material";
 import React, { useEffect } from "react";
 import useHeroStore from "../../store/heroStore";
@@ -58,86 +59,93 @@ export default function AdminCards() {
               gap: "30px",
             }}
           >
-            {hero && hero.length > 0 ? (
-              hero.map((item) => (
-                <Card
-                  key={item.id}
-                  sx={{
-                    maxWidth: "1000px",
-                    width: "100%",
-                    background: "#F5FCFF",
-                    borderRadius: "0px",
-                    border: "none",
-                    display: "flex",
-                    p: 1,
-                    flexDirection: "column",
-                  }}
-                >
-                  <CardContent
-                    sx={{
-                      display: "flex",
-                      flexDirection: "column",
-                      alignItems: "center",
-                    }}
-                  >
-                    <CardMedia
-                      component="img"
-                      image={`/image.png`}
+            <Grid2
+              container
+              spacing={{ xs: 2, md: 3 }}
+              columns={{ xs: 4, sm: 4, md: 4 }}
+            >
+              {hero && hero.length > 0 ? (
+                hero.map((item) => (
+                  <Grid2 item key={item.id} xs={6} sm={4} md={3}>
+                    <Card
+                      key={item.id}
                       sx={{
                         width: "100%",
-                        height: "200px",
-                        objectFit: "cover",
-                      }}
-                      loading="lazy"
-                    />
-                    <Typography
-                      sx={{
-                        fontSize: { xs: "2rem", md: "3rem" },
-                        fontWeight: "bold",
-                        mt: 2,
-                        textAlign: "center",
-                      }}
-                    >
-                      {item.fields.fio}
-                    </Typography>
-                    <Typography
-                      color="black"
-                      sx={{
-                        fontSize: { xs: "1rem", md: "1.25rem" },
-                        mt: 1,
-                      }}
-                    >
-                      {item.fields.years}
-                    </Typography>
-                    <Typography
-                      sx={{
-                        color: "#737373",
-                        fontSize: { xs: "1.2rem", md: "1.5rem" },
-                        mt: 2,
-                        textAlign: "center",
-                      }}
-                    >
-                      {item.fields.info}
-                    </Typography>
-                    <Button
-                      sx={{
-                        width: "100%",
+                        background: "#F5FCFF",
                         borderRadius: "0px",
-                        backgroundColor: "#E01D04",
-                        mt: 4,
-                        fontSize: "20px",
+                        border: "none",
+                        display: "flex",
+                        p: 1,
+                        flexDirection: "column",
                       }}
-                      variant="contained"
-                      onClick={() => handleDelete(item.id)}
                     >
-                      Удалить
-                    </Button>
-                  </CardContent>
-                </Card>
-              ))
-            ) : (
-              <Typography>Нет данных для отображения</Typography>
-            )}
+                      <CardContent
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "center",
+                        }}
+                      >
+                        <CardMedia
+                          component="img"
+                          image={`/image.png`}
+                          sx={{
+                            width: "200px",
+                            height: "200px",
+                            objectFit: "cover",
+                          }}
+                          loading="lazy"
+                        />
+                        <Typography
+                          sx={{
+                            fontSize: { xs: "2rem", md: "3rem" },
+                            fontWeight: "bold",
+                            mt: 2,
+                            textAlign: "center",
+                          }}
+                        >
+                          {item.fields.fio}
+                        </Typography>
+                        <Typography
+                          color="black"
+                          sx={{
+                            fontSize: { xs: "1rem", md: "1.25rem" },
+                            mt: 1,
+                          }}
+                        >
+                          {item.fields.years}
+                        </Typography>
+                        <Typography
+                          sx={{
+                            color: "#737373",
+                            fontSize: { xs: "1.2rem", md: "1.5rem" },
+                            mt: 2,
+                            textAlign: "center",
+                          }}
+                        >
+                          {item.fields.info}
+                        </Typography>
+                        <Button
+                          sx={{
+                            width: "100%",
+                            borderRadius: "0px",
+                            backgroundColor: "#E01D04",
+                            mt: 4,
+                            fontSize: "20px",
+                          }}
+                          variant="contained"
+                          onClick={() => handleDelete(item.id)}
+                        >
+                          Удалить
+                        </Button>
+                      </CardContent>
+                    </Card>
+                  </Grid2>
+                ))
+              ) : (
+                <Typography>Нет данных для отображения</Typography>
+              )}
+            </Grid2>
           </Box>
         </Box>
       </Box>
